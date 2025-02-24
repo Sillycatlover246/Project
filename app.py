af4351b4f7ed26ad056738259222ad571861d58d
Noam <Sillycatlover246@gmail.com>
2025-02-24 20:01:04 +0200

    updated logic

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
-app = Flask(__name__) # creates an instance of flask class passing __name__ as an argument
+app = Flask(__name__)
 
-@app.route("/") # @ is a decorator (a function that wraps another function)
-def home(): # just a function called home
-    return render_template("index.html") # pretty much looks for the index.html file in the templates folder
+tasks = []
+
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html", tasks=tasks)
+
+@app.route("/add_task", methods=["POST"])
+def add_task():
+    task_name = request.form.get("task_name")
+    if task_name:
+        tasks.append(task_name)
+    return redirect(url_for("index"))
+
+@app.route("/remove_task/<int:task_index>", methods=["POST"])
+def remove_task(task_index):
+    if 0 <= task_index < len(tasks):
+        tasks.pop(task_index)
+    return redirect(url_for("index"))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True) # just starts the flask dev server in debug mode
+    # Important to specify host="0.0.0.0" for Docker
+    app.run(host="0.0.0.0", port=5000, debug=True)
