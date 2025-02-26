548d3fe384b76142c4625c684efa70018bd123a6
Noam <Sillycatlover246@gmail.com>
2025-02-26 20:57:38 +0200

    added a dash to googlebeta lmao hopefully this works

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,24 +2,24 @@ terraform {
   required_version = ">= 1.3.0"
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "~> 4.0"
     }
-    googlebeta = {
-      source = "hashicorp/googlebeta"
+    google-beta = {
+      source  = "hashicorp/google-beta"
       version = "~> 4.0"
     }
   }
 }
 
 provider "google" {
-  project = var.project_id
-  region  = var.region
+  project     = var.project_id
+  region      = var.region
   credentials = file(var.gcp_credentials_file)
 }
 
-provider "googlebeta" {
-  project = var.project_id
-  region  = var.region
+provider "google-beta" {
+  project     = var.project_id
+  region      = var.region
   credentials = file(var.gcp_credentials_file)
 }
