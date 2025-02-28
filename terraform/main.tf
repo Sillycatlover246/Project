4000454da59d9229422921ca9ebb61762f361b8d
Noam <Sillycatlover246@gmail.com>
2025-02-28 19:57:34 +0200

    all this fleeting

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,28 +1,35 @@
 resource "google_container_cluster" "primary" {
   name     = var.cluster_name
   location = "us-central1-a"
+  project  = "devproject-451919"  # Explicitly set project ID
   
-  # These should match your existing cluster's configuration
-  # Remove this if your existing cluster has a different configuration
+  # Set these to match your existing cluster
+  # If Terraform can't import, it might try to create/modify
   initial_node_count = 1
   remove_default_node_pool = true
 
   network    = "default"
   subnetwork = "default"
   
-  # Important: This tells Terraform not to try changing the cluster on every run
+  # Critical: This prevents Terraform from trying to manage aspects 
+  # of the cluster that might differ from your config
   lifecycle {
     ignore_changes = [
-      node_config,
       initial_node_count,
+      node_config,
+      master_auth,
+      ip_allocation_policy,
+      enable_autopilot,
       node_pool,
+      binary_authorization,
+      remove_default_node_pool,
     ]
   }
 }
 
 resource "google_container_node_pool" "primary_nodes" {
   name       = "${var.cluster_name}-node-pool"
-  project    = var.project_id
+  project    = "devproject-451919"  # Explicitly set project ID
   location   = "us-central1-a"
   cluster    = google_container_cluster.primary.name
   node_count = var.node_count
@@ -35,11 +42,14 @@ resource "google_container_node_pool" "primary_nodes" {
     ]
   }
   
-  # This prevents Terraform from recreating the node pool on every run
+  # This prevents Terraform from modifying existing node pool config
   lifecycle {
     ignore_changes = [
       node_count,
       node_config,
+      version,
+      management,
+      upgrade_settings,
     ]
   }
 }
