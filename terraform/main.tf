ead059e2ef4c8c28c472bc9a31c41f419cb4a850
Noam <Sillycatlover246@gmail.com>
2025-02-28 17:19:42 +0200

    regional to zonal

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,20 +1,19 @@
 resource "google_container_cluster" "primary" {
   name     = var.cluster_name
-  location = var.region
+  location = "us-central1-a" # switched from regional to zonal
 
-  # This is a "regional" cluster if location is region; or use "zone" if you want a zonal cluster
   initial_node_count = 1
   remove_default_node_pool = true
 
   network    = "default"
   subnetwork = "default"
-  # By default, the cluster is public. You can lock it down or use private clusters if you prefer
+  # public cluster11!
 }
 
 resource "google_container_node_pool" "primary_nodes" {
   name       = "${var.cluster_name}-node-pool"
   project    = var.project_id
-  location   = var.region
+  location   = "us-central1-a"
   cluster    = google_container_cluster.primary.name
   node_count = var.node_count
 
