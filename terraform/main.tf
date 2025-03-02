59bce095ed0e135466f6b5e886982cf208e22387
Noam <Sillycatlover246@gmail.com>
2025-03-02 11:00:41 +0200

    leefa syclae

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 resource "google_container_cluster" "primary" {
   name                      = var.cluster_name
-  location                  = "us-central1-a"   # Use a specific zone
+  location                  = "us-central1-a"   # Zonal cluster in us-central1-a
   initial_node_count        = 1
   remove_default_node_pool  = true
   network                   = "default"
@@ -17,10 +17,17 @@ resource "google_container_node_pool" "primary_nodes" {
   node_config {
     machine_type = var.node_machine_type
     disk_size_gb = 80
-    disk_type    = "pd-ssd"          # **Added disk_type attribute**
-    image_type   = "COS_CONTAINERD"  # or "UBUNTU_CONTAINERD" if preferred
+    disk_type    = "pd-ssd"
+    image_type   = "COS_CONTAINERD"
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+
+  lifecycle {
+    # Ignore any changes to node_config because GKE sets many computed defaults
+    ignore_changes = [
+      node_config,
     ]
   }
 }
